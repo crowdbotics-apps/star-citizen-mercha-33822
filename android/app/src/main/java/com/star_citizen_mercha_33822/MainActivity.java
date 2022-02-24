7eb0e7dfea2cf9ef9a2038fde8d2b7c7d46b979f
Crowdbotics <team@crowdbotics.com>
2022-02-24 14:53:34 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/star_citizen_mercha_33822/MainActivity.java
@@ -0,0 +1,15 @@
+package com.star_citizen_mercha_33822;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "star_citizen_mercha_33822";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "star_citizen_mercha_33822",
+  url: "https://star_citizen_mercha_33822.botics.co",
+  api: "https://star_citizen_mercha_33822.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
